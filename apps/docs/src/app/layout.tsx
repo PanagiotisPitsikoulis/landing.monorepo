e68414e2f499c0186efdd5d0b516684d2663c1d5
Panagiotis Pitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2025-01-01 17:17:08 +0200

    bug fix

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// TODO Fix monorepo imports
-// import { LandingUIProvider } from "@repo/ui/utils";
+import { LandingUIProvider } from "@repo/ui/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <LandingUIProvider> */}
-        <main>{children}</main>
-        {/* </LandingUIProvider> */}
+        <LandingUIProvider>
+          <main>{children}</main>
+        </LandingUIProvider>
       </body>
     </html>
   );
