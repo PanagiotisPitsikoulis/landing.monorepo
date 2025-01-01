26fb3d5570877ebcc49ef164c37bfb73c18f6f46
Panagiotis Pitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2025-01-01 20:25:02 +0200

    bug fixes ui

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import Link from "next/link";
+import { ThemeSwitch } from "ui.landing";
 
 export default function Home() {
   return (
     <main>
+      <ThemeSwitch />
       <Link href='/docs/get_started'>Go to docs</Link>
     </main>
   );
