db5fb7b8ef5b5879245aa0998c20f173526d9ecd
Panagiotis Pitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2025-01-01 22:12:30 +0200

    prod

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { NavbarMenuToggle } from "ui.landing";
+import { ThemeSwitch } from "ui.landing";
 
 export default function Home() {
   return (
     <main>
+      <ThemeSwitch />
       <Link href='/docs/get_started'>Go to docs</Link>
     </main>
   );
