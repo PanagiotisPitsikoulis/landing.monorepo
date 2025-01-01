b4d1bf811d8ab226d04e313b177322024621264e
Panagiotis Pitsikoulis <102904497+PanagiotisPitsikoulis@users.noreply.github.com>
2025-01-01 18:27:16 +0200

    bug fix

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,22 +1,14 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
-import { nextui } from "@repo/ui/nextui";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/landing.ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/**/*.{js,ts,jsx,tsx,mdx}",
     "../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
   darkMode: "class",
   plugins: [nextui()],
 } satisfies Config;
